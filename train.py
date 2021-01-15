2942ea9f399554dc3fe2b59057d826c246bd9976
Nick Corona <nickcorona@users.noreply.github.com>
2021-01-14 22:04:51 -0500

    quotes

--- train.py
+++ train.py
@@ -306,7 +306,7 @@ model = lgb.train(
 )
 
 lgb.plot_importance(model, importance_type="gain", grid=False, figsize=(10, 5))
-plt.savefig('figures/feature_importance.png')
+plt.savefig("figures/feature_importance.png")
 
 from sklearn.metrics import accuracy_score
 
